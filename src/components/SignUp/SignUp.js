f5261f4082b33e801361b973e84845b73be5a989
nizamuddin15bd <nizamuddin15bd@gmail.com>
2023-03-22 19:39:41 +0600

    sign up and login

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const SignUp = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [confirmPassword, setConfirmPassword] = useState('')
     const [error, setError] = useState('')
+    const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth);
+    const navigate = useNavigate()
 
     const handleEmailBlur = event => {
         setEmail(event.target.value);
@@ -16,12 +20,20 @@ const SignUp = () => {
     const handleConfirmPasswordBlur = event => {
         setConfirmPassword(event.target.value);
     };
+    if (user) {
+        navigate('/shop')
+    }
     const handleCreateUser = event => {
         event.preventDefault();
         if (password !== confirmPassword) {
             setError('Your to Password did not match')
             return
         }
+        if (password.length < 6) {
+            setError('password must be 6 characters or longer')
+            return;
+        }
+        createUserWithEmailAndPassword(email, password);
     }
     return (
         <div className='form-container'>
